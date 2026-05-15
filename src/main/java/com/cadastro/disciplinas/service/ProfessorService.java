b1f8132f6bf9478811acb8cd5337defbb2f5b735
PPG2006 <pedro.p.gatto2006@gmail.com>
2026-05-15 16:17:47 -0300

    Update ProfessorService.java

--- src/main/java/com/cadastro/disciplinas/service/ProfessorService.java
+++ src/main/java/com/cadastro/disciplinas/service/ProfessorService.java
@@ -3,7 +3,7 @@ package com.cadastro.disciplinas.service;
 import com.cadastro.disciplinas.domain.model.Professor;
 import com.cadastro.disciplinas.domain.repository.ProfessorRepository;
 
-public class ProfessorService {
+public class ProfessorService implements ProfessorRepository{
     private final ProfessorRepository professorRepository;
 
     // Injeção de dependência via construtor
@@ -15,4 +15,40 @@ public class ProfessorService {
         // Implementar regras futuras antes de salvar
         professorRepository.salvar(professor);
     }
-}
+
+    @Override
+    public void salvar(Professor professor){
+        professores.add(professor); 
+    }
+
+    @Override
+    public void buscarPorCodigo(Long codigoFuncional){
+        for (Professor p : professores){
+            if(p.getCodigoFuncional().equals(codigoFuncional)){
+                System.out.println("Professor " + p.getNome() + " encontrado");
+            }
+        }
+    }
+
+     @Override
+    public void listarTodos(){
+        for (Professor p : professores){
+            System.out.println("Professor " + p.getNome() + "\n");
+        }
+    }
+
+     @Override
+    public void deletar(Long codigoFuncional){
+        Iterator<Professor> iterator = professores.iterator();
+
+        while (iterator.hasNext()) {
+            Professor p = iterator.next();
+            if (p.getCodigoFuncional().equals(codigoFuncional)) {
+                iterator.remove();
+                System.out.println("Professor removido com sucesso!");
+                return;
+            }
+        }
+            System.out.println("Professor não encontrado.");
+        }
+    }
